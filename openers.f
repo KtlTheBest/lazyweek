7c002985c3c7c07c310991cd3cfefad77bedd50d
marik <marik@localhost.localdomain>
2020-05-11 17:14:52 +0600

    Add keywords and phrases

---
+++ openers.f
@@ -0,0 +1,5 @@
+On my first week we did KEYWORD.;On our first week we did KEYWORD.;This
+week we decided to finish the important tasks from the last week.;This
+week's topic is KEYWORD.;This week was very helpful in terms of
+progress.;This week my main focus was on KEYWORD.;This week I did
+several tasks oriented on KEYWORD.
---
+++ phrases.f
@@ -0,0 +1,34 @@
+I studied KEYWORD to learn how to solve KEYWORD problem.;Our team was
+given a task to solve the problem of KEYWORD in order to achieve KEYWORD
+on our KEYWORD.; We scheduled KEYWORD on everyday basis to help us
+better perform KEYWORD.; I discovered several KEYWORD for myself which I
+will probably use for myself.; At the end of the week I learned about KEYWORD
+which is incredible. I can't believe that I didn't know about such thing
+even existing.;We also covered interesting topics, such as KEYWORD and
+KEYWORD.;My mentor asked me to perform KEYWORD and see if I can KEYWORD
+it. It was a piece of cake.;My mentor asked me to perform KEYWORD and see if I can KEYWORD
+it. I couldn't do it, because I still lacked KEYWORD skills to ace the
+task.;There were intersting topics, such as KEYWORD, KEYWORD and
+KEYWORD.;There were interesting topics suck as KEYWORD and
+KEYWORD.;Something that piqued my interest was KEYWORD, because I never
+knew that I can use it to KEYWORD.;As a result we finished everything on
+time.;It was a very productive week.;Mentor gave me a really difficult
+task to work with.;One takes a KEYWORD and tries to apply KEYWORD to get
+KEYWORD as a result.;KEYWORD is also designed to help KEYWORD in
+KEYWORD.;Next week will try to finish the KEYWORD we started this week,
+but couldn't finish.;This week the weather was nice.;One of the main
+tasks for the week was to KEYWORD.;The reason for that decision was
+really profound.;I learned how to KEYWORD for the first time.;I
+encountered a really difficult problem.;My colleagues don't know how to
+solve it right know.;Today I learned that KEYWORD is used in enterprise
+companies.;The KEYWORD is a tool for automating a KEYWORD.;The KEYWORD
+is a method to ease the process of KEYWORD.;The basics of KEYWORD
+include KEYWORD, KEYWORD and KEYWORD.;I really don't like writing
+essays.;The interesting part comes after KEYWORD.;The mentor told me how
+to do it.;We had a tasty pizza.;My mentor's birthday was yesterday. We
+had a lot of pizza.;One of the responsibilities includes working with
+other departments.;I don't know what I would've done if there wasn't any
+Google nearby.;By using the method of KEYWORD, we can easily avoid
+unnescessary workload.;One of the best practices is to Keep It Simple,
+Stupid.;One of the colleagues came from the conference somewhere in the
+America and brought a lot of stickers.;KEYWORD depends on KEYWORD.;
